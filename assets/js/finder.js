72b5b67b0cc1bb3002b437e2d7db441201438ba8
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-28 16:41:40 -0500

    badge for fork, star n watchers

--- assets/js/finder.js
+++ assets/js/finder.js
@@ -13,6 +13,36 @@ $(document).ready(function() {
         client_secret: "69d80d1c3e9b13529d1cc601072f7ba65ba7a01d"
       }
     }).done(function(response) {
+      $.ajax({
+        url: "https://api.github.com/users/" + userName + "/repos",
+        data: {
+          client_id: "3276363d77933931f154",
+          client_secret: "69d80d1c3e9b13529d1cc601072f7ba65ba7a01d",
+          sort: "create: asc",
+          per_page: 5
+        }
+      }).done(function(repos) {
+        $.each(repos, function(index, repo) {
+          $("#repos").append(`
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-7">
+                        <strong>${repo.name}</strong>: ${repo.description}
+                    </div>
+                    <div class="col-md-3">
+                        <span class="badge badge-dark">Forks: ${repo.forks_count}</span>
+                        <span class="badge badge-primary">Watchers: ${repo.watchers_count}</span>
+                        <span class="badge badge-success">Stars: ${repo.stargazers_count}</span>
+                    </div>
+                    <div class="col-md-2">
+                        <a href="${repo.html_url}" target="_blank" class="btn btn-dark">Repo Page</a>
+                    </div>
+                </div>
+            </div>
+            `);
+        });
+      });
+
       //   console.log(response);
       $("#profile").html(`
 <div class="card card-deafult">
@@ -44,6 +74,9 @@ $(document).ready(function() {
             </div>
         </div>
       </div>
+      <br>
+        <h3 class="page-header">Latest Repos</h3>
+        <div id="repos"></div>
         `);
     });
   });
